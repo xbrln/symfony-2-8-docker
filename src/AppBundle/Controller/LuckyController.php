abd6f4b41cd7602642b41a2aab1e6dcb2387730a
jeffy <j.kurisingal@holidaypirates.com>
2019-04-21 21:49:04 +0200

    Add table and migrations

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\RandomNumber;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
@@ -17,5 +19,20 @@ class LuckyController
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
+    }
+
+    /**
+     * @Route("/lucky/number/create/{number}")
+     */
+    public function createAction($number)
+    {
+        $randomNumber = new RandomNumber();
+        $randomNumber->setNumber($number);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($randomNumber);
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$randomNumber->getId());
     }
 }
---
+++ src/AppBundle/Entity/RandomNumber.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RandomNumber
+ *
+ * @ORM\Table(name="random_number")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RandomNumberRepository")
+ */
+class RandomNumber
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=255)
+     */
+    private $number;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param string $number
+     *
+     * @return RandomNumber
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return string
+     */
+    public function getnumber()
+    {
+        return $this->number;
+    }
+}
+
